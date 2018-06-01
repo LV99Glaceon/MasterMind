e6b52748c429abc5451f4c333b165e4834bb0b41
Re: Software Intern <30954489+resoftware-intern@users.noreply.github.com>
2018-06-01 13:27:42 +0200

    Changes Master

---
+++ MasterMind/src/MasterMind.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MasterMind extends JFrame {
+
+	  public static void main( String args[] ) {
+	    JFrame frame = new MasterMind();
+	    frame.setSize( 400, 200 );
+	    frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+	    frame.setTitle( "MasterMind" );
+	    JPanel paneel = new Game();
+	    frame.setContentPane( paneel );
+	    frame.setVisible( true );
+	  }
+	}
