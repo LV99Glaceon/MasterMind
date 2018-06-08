d716f5a99e1bb4505cb7aa4559875d4bef99c197
DESKTOP-0HFV3SC\Danique Loman <daniqueloman@gmail.com>
2018-06-08 12:09:48 +0200

    uguii
    nl

--- MasterMind/src/ColourPins.java
+++ MasterMind/src/ColourPins.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
+import java.util.*;
 
 public class ColourPins {
+   public static void main(String[] args) {
+      
+    ArrayList<String> colour_list = new ArrayList<String>(5);
 
-}
+
+    colour_list.add("White");
+    colour_list.add("Black");
+    colour_list.add("Red");
+    colour_list.add("Blue");
+	colour_list.add("Yellow");
+	colour_list.add("Green");
+	    
+    for (int i = 0; i < 6; i++)
+      {
+         System.out.println(colour_list.get(i).toString());
+      }
+    
+  }
+} 
--- MasterMind/src/MasterMind.java
+++ MasterMind/src/MasterMind.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class MasterMind extends JFrame {
 
-	  public static void main( String args[] ) {
+	  public static void main( String[] args ) {
 	    JFrame frame = new MasterMind();
 	    frame.setSize( 400, 200 );
 	    frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
